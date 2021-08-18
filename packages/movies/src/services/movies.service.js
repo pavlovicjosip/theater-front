1a06c15458f0411d7100c9a01500746beb781573
Josip Pavlovic <josip.pavlovic@aktivis.hr>
2021-08-18 20:30:05 +0200

    build commit

--- packages/movies/src/services/movies.service.js
+++ packages/movies/src/services/movies.service.js
@@ -3,6 +3,7 @@ export function getGenres() {
 		'https://api.themoviedb.org/3/genre/movie/list?api_key=41f5089313405dd26504221f6ab241c6&language=en-US'
 	).then((data) => data.json());
 }
+
 export function getTopRatedMovies() {
 	return fetch(
 		'https://api.themoviedb.org/3/movie/top_rated?api_key=41f5089313405dd26504221f6ab241c6&language=en-US&page=1'
