c960db33c0f8c245ebd9b1f2b24e100d0823ae1b
Josip Pavlovic <josip.pavlovic@aktivis.hr>
2021-08-18 16:11:21 +0200

    fix style for button

--- packages/movies/src/components/MovieCard.js
+++ packages/movies/src/components/MovieCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { IconButton } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
-
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
